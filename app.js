890ba235fbed3ee5a7e326e6dde23138ebe292e4
unknown <you@example.com>
2024-01-31 17:37:31 +0700

    api and db set up

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var connectDB = require('./models/Database');
+var loginRouter = require("./routes/login.js");
 
 var app = express();
+
+// Kết nối đến MongoDB
+connectDB();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/login", loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
