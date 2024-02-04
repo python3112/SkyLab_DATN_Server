129ba4be2e4b0547683bc22b22d11155cb0d2e47
python3112 <hoangporon3112@gmail.com>
2024-02-04 09:53:39 +0700

    reset project 2

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ dotenv.config();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var connectDB = require('./models/Database');
-var loginRouter = require("./routes/login.js");
+
 
 var app = express();
 
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use("/login", loginRouter);
+
 
 
 // catch 404 and forward to error handler
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 const { Account } = require('../models/Account');
 var router = express.Router();
-var api = require('../ApiClient/AccountApi');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
