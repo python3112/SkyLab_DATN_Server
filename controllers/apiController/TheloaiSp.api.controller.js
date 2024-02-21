a2d1bca0865b953764de9d6f27c43108588c4c5d
unknown <you@example.com>
2024-02-21 11:21:31 +0700

    update CRUD
    CRUD SanPham , TheloaiSp , routes

---
+++ controllers/apiController/TheloaiSp.api.controller.js
@@ -0,0 +1,84 @@
+var accoutModel = require('../../models/Account');
+
+exports.listAccounts = async (req, res, next) => {
+    try {
+      const accounts = await accoutModel.find().sort({ _id: -1 });
+      if (accounts.length > 0) {
+        res.json({ status: 200, msg: "Lấy dữ liệu thành công", data: accounts });
+      } else {
+        res.json({ status: 204, msg: "Không có dữ liệu", data: [] });
+      }
+    } catch (err) {
+      res.json({ status: 500, msg: err.message, data: [] });
+    }
+  };
+  
+  exports.createAccount = async (req, res, next) => {
+    try {
+      const newAccount = new Account({
+        nameTk: req.body.taiKhoan,
+        passTk: req.body.matKhau,
+        quyenTc: req.body.quyenTk,
+        avavta: req.body.avatar,
+        ttTaiKhoan: req.body.trangThai || false,
+      });
+  
+      await newAccount.save();
+      res.json({ status: 200, msg: "Thêm tài khoản thành công" });
+    } catch (err) {
+      res.json({ status: 500, msg: err.message });
+    }
+  };
+  
+  exports.getAccountByName = async (req, res, next) => {
+    try {
+      const account = await accoutModel.findOne({ nameTk: req.params.taiKhoan });
+      if (account) {
+        res.json({ status: 200, msg: "Lấy dữ liệu thành công", data: account });
+      } else {
+        res.json({ status: 204, msg: "Không tìm thấy tài khoản", data: null });
+      }
+    } catch (err) {
+      res.json({ status: 500, msg: err.message, data: null });
+    }
+  };
+  
+  exports.getAccountById = async (req, res, next) => {
+    try {
+      const account = await accoutModel.findById(req.params.id);
+      if (account) {
+        res.json({ status: 200, msg: "Lấy dữ liệu thành công", data: account });
+      } else {
+        res.json({ status: 204, msg: "Không tìm thấy tài khoản", data: null });
+      }
+    } catch (err) {
+      res.json({ status: 500, msg: err.message, data: null });
+    }
+  };
+  
+  exports.updateAccount = async (req, res, next) => {
+    try {
+      const updatedData = {
+        nameTk: req.body.taiKhoan,
+        passTk: req.body.matKhau,
+        quyenTc: req.body.quyenTk,
+        avavta: req.body.avatar,
+        ttTaiKhoan: req.body.trangThai || true,
+      };
+  
+      await Account.updateOne({ _id: req.params.id }, updatedData);
+      res.json({ status: 200, msg: "Sửa thông tin tài khoản thành công" });
+    } catch (err) {
+      res.json({ status: 500, msg: err.message });
+    }
+  };
+  
+  exports.deactivateAccount = async (req, res, next) => {
+    try {
+      await Account.updateOne({ _id: req.params.id }, { trangThai: false });
+      res.json({ status: 200, msg: "Vô hiệu hóa tài khoản thành công" });
+    } catch (err) {
+      res.json({ status: 500, msg: err.message });
+    }
+  };
+  
