49c215f32c19eaba6f94657bd46436fca74f2b4c
python3112 <hoangporon3112@gmail.com>
2024-03-18 17:25:00 +0700

    sdsa

--- routes/api/Mess.api.router.js
+++ routes/api/Mess.api.router.js
@@ -10,7 +10,7 @@ const upload = multer({storage :  storage});
 router.post('/send' , MessCtrl.CreateMess);
 router.post('/file', upload.any('image') , MessCtrl.CreateMessWithFile);
 router.get('/' , MessCtrl.getChats);
-router.put('/seen' , MessCtrl.putSeen);
+router.put('/seen/:id' , MessCtrl.putSeen);
 router.put('/revoke/:id' ,MessCtrl.revokeMess);
 router.delete('/:id' , MessCtrl.deleteMess)
 
