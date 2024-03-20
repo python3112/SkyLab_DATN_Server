8ae86bb43ff5a91fe0345df865e8d2dd30683db9
hieuchu <chudinhieuytty@gmail.com>
2024-03-20 23:12:55 +0700

    trang home sản phẩm

--- controllers/sanpham.controller.js
+++ controllers/sanpham.controller.js
@@ -1,4 +1,10 @@
+const { SanPham } = require('../models/SanPham');
 
-exports.home = (req,res,next)=>{
-    res.render('sanpham/home_sanpham',{title: "Quản lý sản phẩm"});
+exports.home =async (req,res,next)=>{
+    try {
+        const sanPham = await SanPham.find();
+        res.render('sanpham/home_sanpham',{title: "Quản lý sản phẩm",listSanPham: sanPham});
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 }
