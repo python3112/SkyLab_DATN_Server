87f3a86a886a422a0e9253c1b4858287ec7b384c
chudinhhieu <chudinhhieuytty@gmail.com>
2024-03-25 16:19:47 +0700

    Màn hình chi tiết sản phẩm

--- controllers/sanpham.controller.js
+++ controllers/sanpham.controller.js
@@ -8,3 +8,12 @@ exports.home =async (req,res,next)=>{
         res.status(500).json({ message: error.message });
     }
 }
+
+exports.chiTiet = async (req, res) => {
+    try {
+        const sanPham = await SanPham.findById(req.params.id);
+        res.render('sanpham/chitiet_sanpham',{title: "Chi tiết sản phẩm",sanPham: sanPham});
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
