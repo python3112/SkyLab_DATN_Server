6c5bba8da267388ae113a912e1932e51b6191ae2
hieuchu <chudinhieuytty@gmail.com>
2024-03-27 23:00:04 +0700

    Phân trang, lọc đơn hàng theo trạng thái

--- routes/donhang.router.js
+++ routes/donhang.router.js
@@ -5,11 +5,6 @@ var donhangCtrl = require('../controllers/donhang.controller');
 
 // Vào trang home theo địa chỉ '/'
 router.get('/',donhangCtrl.home);
-router.get('/cho-xac-nhan',donhangCtrl.layChoXacNhan);
-router.get('/cho-giao-hang',donhangCtrl.layChoGiaoHang);
-router.get('/dang-giao-hang',donhangCtrl.layDangGiaoHang);
-router.get('/da-giao-hang',donhangCtrl.layDaGiaoHang);
-router.get('/da-huy',donhangCtrl.layDaHuy);
 router.get('/chi-tiet/:id',donhangCtrl.chitiet);
 router.post('/chi-tiet/:id',donhangCtrl.themTrangThaiPost);
 
