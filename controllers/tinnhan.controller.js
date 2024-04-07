ded7eb4417e56b350009cc8c5de6088ef2939746
python3112 <hoangporon3112@gmail.com>
2024-04-07 09:36:30 +0700

    dsds

--- controllers/tinnhan.controller.js
+++ controllers/tinnhan.controller.js
@@ -1,36 +1,40 @@
 const { realtimeDatabase } = require('../middlewares/firebase.config');
 const Account = require('../models/Account');
+
+
+
+
 exports.home = async (req, res, next) => {
     try {
-        // var listAccounts = [];
-        // var lisMess = [];
-        // realtimeDatabase.ref('/Chat').on('value', async (snapshot) => {
-        //     const dataArray = Object.values(snapshot.val());
-        //     for (const element of dataArray) {
-        //         let user = await Account.findById(element.Nguoigui);
-        //         console.log(user)
-        //         const check = await realtimeDatabase.ref('/messages')
-        //         .orderByChild('idChat')
-        //         .equalTo(element.id)
-        //         .once('value')
-        //         if(check.exists()){
-        //             const messArray = Object.values(check.val());
-        //             listAccounts.push(user)
-        //             lisMess.push(messArray[0])
-                    
-        //         }
-               
-                
-               
-        //         console.log(listAccounts)
-        //         console.log(lisMess)
-        //     } 
+        var listAccounts = [];
+        var lisMess = [];
+        realtimeDatabase.ref('/Chat').once('value', async (snapshot) => {
+            const dataArray = Object.values(snapshot.val());
+            for (const element of dataArray) {
+                let user = await Account.findById(element.Nguoigui);
+                listAccounts.push(user)
+                const check = await realtimeDatabase.ref('/messages')
+                    .orderByChild('idAccount')
+                    .equalTo(element.Nguoigui)
+                    .once('value')
+                if (check.exists()) {
+                    const messArray = Object.values(check.val());    
+                    lisMess.push(messArray.at(messArray.length - 1))
+
+                }
+
+
+            }
+            console.log(lisMess)
+
             
-            
-            await res.render('tinnhan/home_tinnhan', { title: 'màn hình nhắn tin'});
-        // });
+            await res.render('tinnhan/home_tinnhan', { title: 'màn hình nhắn tin' , listAccounts :  listAccounts , listMess :lisMess });
+        });
+      
+       
 
     } catch (error) {
         console.log(error)
     }
 }
+
